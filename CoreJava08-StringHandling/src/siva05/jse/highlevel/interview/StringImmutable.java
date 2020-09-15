97b7565ec0fa650fb5b00cbc9c1bf6d9d7345c6d
sivam <sivam@ANNAPURNA>
2020-09-15 20:20:12 +0530

    15-Sep-2020

---
+++ CoreJava08-StringHandling/src/siva05/jse/highlevel/interview/StringImmutable.java
@@ -0,0 +1,12 @@
+package siva05.jse.highlevel.interview;
+
+public class StringImmutable {
+
+	public static void main(String[] args) {
+		String name = "SIVA";
+		name.toLowerCase();
+		name = name + "KUMAR";
+		System.out.println(name);
+	}
+
+}
